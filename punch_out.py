68f9cdd652068da69130026e32a3773131734645
Manoj Kumar <mkumar6@ch.iitr.ac.in>
2025-03-02 16:27:39 +0530

    Added Github Workflows
    Signed-off-by: Manoj Kumar <mkumar6@ch.iitr.ac.in>

--- punch_out.py
+++ punch_out.py
@@ -1,23 +1,32 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
+# Load credentials from GitHub Secrets
+USERNAME = os.getenv("FACTOHR_USERNAME")
+PASSWORD = os.getenv("FACTOHR_PASSWORD")
+
+if not USERNAME or not PASSWORD:
+    raise ValueError("FACTOHR_USERNAME or FACTOHR_PASSWORD is missing!")
+
 # Set up WebDriver
-driver = webdriver.Chrome()  # Ensure chromedriver is installed
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  # Run in headless mode (no UI)
+driver = webdriver.Chrome(options=options)
 
 # Open factoHR
 driver.get("https://app2.factohr.com/tspl/Security/Login")
 
 # Login
-driver.find_element(By.ID, "txtUsername").send_keys("A993")
-driver.find_element(By.ID, "txtPassword").send_keys("Log@123#")
+driver.find_element(By.ID, "txtUsername").send_keys(USERNAME)
+driver.find_element(By.ID, "txtPassword").send_keys(PASSWORD)
 driver.find_element(By.ID, "btnLogin").click()
 
 # Wait for login to complete
 time.sleep(15)
 
-# # Click Punch-In
+# Click Punch-Out
 driver.find_element(By.ID, "Punch Out").click()
 
 # Close browser
